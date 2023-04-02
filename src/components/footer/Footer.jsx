14fbeb7e3f39bc78a82919fede7e4a71665ad91c
Bernard Hechter <bernardh0602@gmail.com>
2023-04-02 16:01:55 +0200

    footer element fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,13 +2,13 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="attribution">
+    <footer className="attribution">
       Challenge by{" "}
       <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
         Frontend Mentor
       </a>
       . Coded by <a href="https://www.frontendmentor.io/profile/BernardusPH">Bernard</a>.
-    </div>
+    </footer>
   );
 }
 
