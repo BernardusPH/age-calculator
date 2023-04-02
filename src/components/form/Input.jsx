4cb296f954c9aad30eab0230cf9eb9246b00f4c7
Bernard Hechter <bernardh0602@gmail.com>
2023-04-02 16:45:02 +0200

    input step removal

--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -5,7 +5,7 @@ function Input(props) {
   return (
     <div className={classes["input-wrapper"]}>
       <label className={`${classes["input-label"]} ${props.errorText==""?classes["input-label-normal"]:classes["input-label-error"]}`} htmlFor={props.id}>{props.label}</label>
-      <input step="any" className={`${classes["user-input"]} ${props.errorText==""?classes["user-input-normal"]:classes["user-input-error"]}`} onChange={ props.errorClear} ref={props.inputReference} type="number" id={props.id} placeholder={props.placeholder} />
+      <input className={`${classes["user-input"]} ${props.errorText==""?classes["user-input-normal"]:classes["user-input-error"]}`} onChange={ props.errorClear} ref={props.inputReference} type="number" id={props.id} placeholder={props.placeholder} />
       <span className={classes["error-text"]}>{props.errorText}</span>
     </div>
   );
